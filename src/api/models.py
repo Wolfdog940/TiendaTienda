19b9067b156f2dcef2b55ce49cba0567bff493dc
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-09-23 18:01:00 +0000

    tablas

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,9 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    carts = db.relationship('Product', backref='users', lazy=True)
+    favorites = db.relationship('Product', secondary=favorites, lazy='subquery',
+        backref=db.backref('users', lazy=True))
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +18,57 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorites": list(map(lambda favorite:favorite.serialize(),self.favorites))
+            
+            
         }
+
+
+class Product (db.model): 
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String , unique=False, nullable=False)
+    description = db.Column(db.String(240) , unique=False, nullable=False)
+    price = db.Column(db.integer , unique=False, nullable=False)
+    category= db.Column(db.String , unique=False, nullable=True)
+    stock =db.Column(db.String , unique=False, nullable=True)
+    carts = db.relationship('Cart', backref='products', lazy=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "category": self.category,
+            "stock": self.stock,
+
+            
+        }
+
+class Cart (db.model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.integer , db.ForeignKey('user.id'),
+        nullable=False)
+    product_id= db.Column(db.integer , db.ForeignKey('product.id'),
+        nullable=False)
+    amount = db.Column(db.integer , unique=False, nullable=False),
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "prouct_id": self.product_id,
+            "user_id": self.user_id,
+            "amount": self.amount,
+
+        }
+
+
+
+
+
+favorite  = db.Table('favorite',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+)
